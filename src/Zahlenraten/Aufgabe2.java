caa81d088f6736e54fc9870d7b1ea65f8605277b
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-23 12:40:28 +0100

    Aufgabe3

--- src/Zahlenraten/Aufgabe2.java
+++ src/Zahlenraten/Aufgabe2.java
@@ -12,14 +12,30 @@ public class Aufgabe2
 
         Random rnd = new Random();
         int zufallszahl = rnd.nextInt(max - min + 1) + min;
-        System.out.println(zufallszahl);
+        // System.out.println(zufallszahl);
         /* mit dem Code wird angegeben von welcher Zahl aus der Rechner starten muss und mit welcher Zahl er enden muss
             mit dem rnd.nextInt wird gesagt, dass er es per Zufallsgenerator erledigen soll */
 
-        if ((zufallszahl % 2) == 0) {
+        /* if ((zufallszahl % 2) == 0) {
             JOptionPane.showInputDialog(null, zufallszahl + " ist gerade!");
         } else {
             JOptionPane.showInputDialog(null, zufallszahl + "ist nicht gerade!");
+        }*/
+        String rueckgabe;
+        rueckgabe = JOptionPane.showInputDialog(null, "Gib mir eine Ganzzahl, die zwischen 1 und 10 liegt!");
+
+        if (Integer.parseInt(rueckgabe) == zufallszahl)
+        {
+            JOptionPane.showMessageDialog(null, "Deine Zahl ist gleich!");
+        }
+
+        else if ((Integer.parseInt(rueckgabe)-zufallszahl) >= -2 && (Integer.parseInt(rueckgabe)-zufallszahl)<=2)
+        {
+            JOptionPane.showMessageDialog (null, "Deine Zahl ist innerhalb der Grenze +2 oder -2!");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "Deine Zahl ist nicht gleich!");
         }
     }
 }
